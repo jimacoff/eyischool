0e63d7362389b4d34a919e6bc34c8db22c4e54e5
Jimoh Abdullahi <eyitayoschool@gmail.com>
2019-10-08 19:43:19 -0700

    First commit

--- webpack.backend.mix.js
+++ webpack.backend.mix.js
@@ -18,10 +18,12 @@ mix.webpackConfig({
         // new webpack.IgnorePlugin(
         //     /^\.\/locale$/, /moment$/,
         // )
-        new webpack.ContextReplacementPlugin(
+        new webpack.ContextReplacementPlugin({
             // The path to directory which should be handled by this plugin
-            /moment[\/\\]locale/, /select2[\/\\]dist[\/\\]js[\/\\]i18n/)
-    ]
+            /moment[\/\\]locale/,
+            /select2[\/\\]dist[\/\\]js[\/\\]i18n/,
+        })
+    ],
 });
 
 mix.js('resources/assets/backend/js/app.js', 'public/js')
